4e55ec31dfb8fcb9eabbcb6e5d0175f403efc392
= <leoncai197@gmail.com>
2018-07-28 11:11:47 -0500

    setting up map functionality of app

--- Texas Running/SecondViewController.swift
+++ Texas Running/SecondViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import MapKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, MKMapViewDelegate, UIPickerViewDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var map: MKMapView!
+    
+    // Regions for the map to zoom into
+    // Format: latitude, longitude, zoom
+    let southCoords = [30.274053, -97.752729, 0.05]
+    let northCoords = [30.299027, -97.732822, 0.05]
+    let westCoords = [30.278906, -97.758396, 0.05]
+    let shoalCreekCoords = [30.303036, -97.742863, 0.05]
+    
+    // The map will zoom into the region of the route you pick
+    @IBOutlet weak var regionsField: UITextField!
+    var regionsPicker = UIPickerView()
+    var regions: [String] = ["South", "North", "West", "Shoal Creek"]
+    var selectedRegion = "South"
+    
+    // Shows the regions dropdown when you click the arrow
+    @IBAction func showRegions(_ sender: UIButton) {
+        regionsField.becomeFirstResponder()
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // initializing the regions picker
+        regionsPicker.delegate = self
+        regionsField.inputView = regionsPicker
     }
 
     override func didReceiveMemoryWarning() {
