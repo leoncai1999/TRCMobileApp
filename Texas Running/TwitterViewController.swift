013a369e9b5ad7361f3b8389efe24b13d9893c2b
= <leoncai197@gmail.com>
2018-08-22 00:00:29 -0500

    fetching tweets from trc twitter account

--- Texas Running/TwitterViewController.swift
+++ Texas Running/TwitterViewController.swift
@@ -7,29 +7,16 @@
 //
 
 import UIKit
+import TwitterKit
 
-class TwitterViewController: UIViewController {
-
+class TwitterViewController: TWTRTimelineViewController, TWTRTweetViewDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        let client = TWTRAPIClient()
+        self.dataSource = TWTRUserTimelineDataSource(screenName: "texasrunning", apiClient: client)
+        self.showTweetActions = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
