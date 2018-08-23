96315e052b66e4abf92cb1c725e3fcc365752b72
= <leoncai197@gmail.com>
2018-08-23 17:02:41 -0500

    storing route poll counts in database

--- Texas Running/FirstViewController.swift
+++ Texas Running/FirstViewController.swift
@@ -18,9 +18,25 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var emailButton: UIButton!
     @IBOutlet weak var conditionLabel: UILabel!
     
+    @IBOutlet weak var southButton: checkBox!
+    @IBOutlet weak var northButton: checkBox!
+    @IBOutlet weak var westButton: checkBox!
+    @IBOutlet weak var shoalButton: checkBox!
     
-    // reference to firebase database
-    let rootRef = Database.database().reference()
+    var routeSelected = "None"
+    // indexes: 0 South, 1 North, 2 West, 3 Shoal 
+    var routePollCounts = ["South" : "0", "North" : "0", "West" : "0", "Shoal" : "0"]
+    
+    // references to firebase database
+    
+    // current winner
+    let routeRef = Database.database().reference().child("route")
+    
+    // counts for each route
+    let southRef = Database.database().reference().child("South")
+    let northRef = Database.database().reference().child("North")
+    let westRef = Database.database().reference().child("West")
+    let shoalRef = Database.database().reference().child("Shoal")
     
     var closeTime = "6:00 PM"
     
@@ -46,19 +62,100 @@ class FirstViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let routeRef = rootRef.child("route")
         routeRef.observe(.value) { (snap: DataSnapshot) in
             self.conditionLabel.text = (snap.value as AnyObject).description
+        }
+        // linking dictionary to database
+        southRef.observe(.value) { (snap: DataSnapshot) in
+            self.routePollCounts["South"] = (snap.value as AnyObject).description
+        }
+        northRef.observe(.value) { (snap: DataSnapshot) in
+            self.routePollCounts["North"] = (snap.value as AnyObject).description
+        }
+        westRef.observe(.value) { (snap: DataSnapshot) in
+            self.routePollCounts["West"] = (snap.value as AnyObject).description
+        }
+        shoalRef.observe(.value) { (snap: DataSnapshot) in
+            self.routePollCounts["Shoal"] = (snap.value as AnyObject).description
         }
     }
     
     @IBAction func southDidTouch(_ sender: UIButton) {
+        if (!southButton.isChecked) {
+            routeSelected = "South"
+        } else {
+            routeSelected = "None"
+        }
+        // deselect all other buttons when one is checked
+        northButton.isChecked = false
+        westButton.isChecked = false
+        shoalButton.isChecked = false
     }
     
     @IBAction func northDidTouch(_ sender: UIButton) {
+        if (!northButton.isChecked) {
+            routeSelected = "North"
+        } else {
+            routeSelected = "None"
+        }
+        southButton.isChecked = false
+        westButton.isChecked = false
+        shoalButton.isChecked = false
     }
     
+    @IBAction func westDidTouch(_ sender: UIButton) {
+        if (!westButton.isChecked) {
+            routeSelected = "West"
+        } else {
+            routeSelected = "None"
+        }
+        southButton.isChecked = false
+        northButton.isChecked = false
+        shoalButton.isChecked = false
+    }
     
+    @IBAction func shoalDidTouch(_ sender: UIButton) {
+        if (!shoalButton.isChecked) {
+            routeSelected = "Shoal"
+        } else {
+            routeSelected = "None"
+        }
+        southButton.isChecked = false
+        northButton.isChecked = false
+        westButton.isChecked = false
+    }
+    
+    @IBAction func submitChoice(_ sender: UIButton) {
+        if (routeSelected != "None") {
+            routeRef.setValue(routeSelected)
+            adjustCount(choice: routeSelected)
+            determineWinner()
+        }
+    }
+    
+    func adjustCount(choice: String) {
+        // increment routes count in database and deselct its box
+        let oldCount = Int(routePollCounts[choice]!)
+        let newCount = oldCount! + 1
+        if (choice == "South") {
+            southRef.setValue(String(newCount))
+            southButton.isChecked = false
+        } else if (choice == "North") {
+            northRef.setValue(String(newCount))
+            northButton.isChecked = false
+        } else if (choice == "West") {
+            westRef.setValue(String(newCount))
+            westButton.isChecked = false
+        } else {
+            shoalRef.setValue(String(newCount))
+            shoalButton.isChecked = false
+        }
+    }
+    
+    func determineWinner() {
+        // sets routeRef to the route with the most votes
+        
+    }
     
     @IBAction func facebookRedirect(_ sender: UIButton) {
         UIApplication.tryURL(urls: [
